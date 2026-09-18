11a8455120b2297b1a11bbe299f8e6fdf86117d6
Micah Reed <mreed_msasd@magnoliascience.org>
2026-09-18 11:42:18 -0700

    ok

--- js/input.js
+++ js/input.js
@@ -7,10 +7,12 @@
    ===================================================================== */
 
 var Input = {
-  left: false,
-  right: false,
-  jump: false,
-  restart: false
+   left: false,  
+  right: false,  
+  jump: false,  
+  restart: false,  
+  enter: false,  
+  breakKey: false  
 };
 
 // Called whenever a key goes DOWN.
@@ -34,4 +36,6 @@ function setKey(key, isDown) {
   if (key === "ArrowRight" || key === "d" || key === "D") { Input.right = isDown; }
   if (key === "ArrowUp"    || key === " " || key === "w" || key === "W") { Input.jump = isDown; }
   if (key === "r" || key === "R") { Input.restart = isDown; }
+  if (key === "Enter") { Input.enter = isDown; }  
+  if (key === "b" || key === "B") { Input.breakKey = isDown; }
 }
